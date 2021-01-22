cac99a05fb5016a32dc75cac4d9a7b87990c0fae
zngsm <qbw0088@gmail.com>
2021-01-22 15:16:27 +0900

    start FE project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-import routes from './routes'
-import store from './vuex/store'
-
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-    routes,
-});
-
 new Vue({
-    router,
-    store,
-    render: h => h(App),
-}).$mount('#app');
-
-
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
