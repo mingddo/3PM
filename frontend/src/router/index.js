2c3380d66abc8a67ff373905dd1dd160cfb6eb94
zngsm <qbw0088@gmail.com>
2021-01-22 16:00:42 +0900

    [chore] Update FE PJT
    - 프론트엔드 라우터 추가
    - api 폴더 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,13 +11,30 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/mypage',
+    name: 'MyPage',
+    component: MyPage
+  },
+  {
+    path: '/newsfeed-personal',
+    name: 'NewsfeedPersonal',
+    component: NewsfeedPersonal
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
 ]
 
 const router = new VueRouter({
