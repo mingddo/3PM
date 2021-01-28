8407c30c9ee946221edf3930f53b1252843a5c8b
SANGWON KIM <tkddnjs120@gmail.com>
2021-01-28 17:22:35 +0900

    [fix] 도메인 수정
    도메인 합치기 위해 똑같이 맞춤

--- backend/src/main/java/com/ssafy/sns/domain/user/UserConfig.java
+++ backend/src/main/java/com/ssafy/sns/domain/user/UserConfig.java
@@ -6,16 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter // 비추천 코드
-@NoArgsConstructor // 기본 생성자
-@Entity
+@Embeddable
 public class UserConfig {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "config_id")
-    private Long id;
 
     // 설정들 넣기
 }
