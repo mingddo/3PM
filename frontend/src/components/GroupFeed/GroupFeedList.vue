d8448f1c543955bcc5501faadc58bcc70bf7c34f
zngsm <qbw0088@gmail.com>
2021-02-02 17:45:55 +0900

    [feat(fe)] Group 피드 페이지 / form  페이지
    - group 피드 리스트
    - group 작성 페이지
    - 추가적인 css 없음
    - 기존 뉴스피드 페이지 클래스 재활용
    
    resolve #S04P13B208-56
    resolve #S04P13B208-57

---
+++ frontend/src/components/GroupFeed/GroupFeedList.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="feed-list">
+    <div>
+      <h2>피드</h2>
+    </div>
+    <div class="feed-create-btn-place">
+      <i class="fas fa-plus feed-create-btn" @click="goToCreateForm">새 피드 작성</i> 
+    </div>
+    <div v-if="feed">
+      <FeedForGroup 
+        v-for="(fd, idx) in feed"
+        :key="idx"
+        :fd="fd"
+      />
+    </div>
+    <div v-else>
+      <h1>아직 피드가 없습니다</h1>
+      <h1>새로 작성해주세요!</h1>
+    </div>
+    <div v-if="last" class="feed-last">
+      <img src="https://img.icons8.com/ios/100/000000/sad-sun.png"/>
+      <h3>
+        모든 피드를 확인했습니다. 새로운 피드를 작성해주세요.
+      </h3>
+      <i class="fas fa-plus feed-create-btn" @click="goToCreateForm">새 피드 작성</i> 
+    </div>
+    <div v-else class="loading">
+      <img class="spinner" src="https://img.icons8.com/material-two-tone/96/000000/spinner--v3.png"/>
+    </div>
+  </section>
+</template>
+
+<script>
+import FeedForGroup from './FeedForGroup.vue';
+export default {
+  components: { FeedForGroup },
+  name: 'GroupFeedList',
+  props: {
+    feed: Array,
+    Category: String,
+    last: Boolean,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    goToCreateForm () {
+      this.$router.push({ name: 'groupnewsfeedform', params: {type: 'NEW' }})
+    },
+  },
+  watch: {},
+};
+</script>
+
+<style>
+.feed-list {
+  border: 1px solid;
+  padding: 20px;
+}
+.feed-box {
+  border: 1px solid;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 10px;
+}
+.feed-box:hover {
+  box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
+}
+.feed-create-btn-place {
+  /* margin-left: auto;0 */
+  text-align: right;
+}
+.feed-create-btn {
+  cursor: pointer;
+  border: 1px solid;  
+  border-radius: 5px;
+  padding: 10px;
+}
+.feed-create-btn:hover {
+  box-shadow: 0px 5px 10px rgba(0,0,0,0.2);
+}
+.feed-last {
+  text-align: center;
+  margin: auto;
+}
+</style>
