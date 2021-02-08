2daa6e4bcb0e1e2b508749bd7b1f7165a96ef689
명도균 <dorosee97@gmail.com>
2021-02-08 16:32:26 +0900

    [feature(fe)] 그룹 페이지, 그룹 생성 페이지 생성
    - 그룹 페이지 컴포넌트 이동

---
+++ frontend/src/components/GroupFeed/GroupCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="https://picsum.photos/200" alt="" />
+    <div>
+      <div>group name</div>
+      <div>회원수18명</div>
+    </div>
+    <div>그룹 소개하는 부분입니다.</div>
+    <div>최신 피드가 n 개 업로드 되었습니다.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    group: Object,
+  },
+};
+</script>
+
+<style></style>
