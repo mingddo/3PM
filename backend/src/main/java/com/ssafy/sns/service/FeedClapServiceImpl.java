9c4d0a728871fcaa6acaa38a2a3182407c59cb00
박성호 <solser12@naver.com>
2021-02-09 15:21:12 +0900

    line fix

--- backend/src/main/java/com/ssafy/sns/service/FeedClapServiceImpl.java
+++ backend/src/main/java/com/ssafy/sns/service/FeedClapServiceImpl.java
@@ -57,6 +57,5 @@ public class FeedClapServiceImpl implements ClapService {
                 .stream()
                 .map(SimpleUserDto::new)
                 .collect(Collectors.toList());
-
     }
 }
