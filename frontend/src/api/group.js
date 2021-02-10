ac0634a3045d3a0c19b0d7a7c6daeb66cf25ab55
명도균 <dorosee97@gmail.com>
2021-02-10 11:03:58 +0900

    [feat(fe)] group.js 작성
    - 그룹 api 작성

---
+++ frontend/src/api/group.js
@@ -0,0 +1,98 @@
+import { AuthorizationInstance } from './index.js'
+
+const instance = AuthorizationInstance();
+
+// get 요청
+function getAllGroup(success, fail) {
+  instance
+    .get('groups')
+    .then(success)
+    .catch(fail);
+}
+
+function getGroupDetail(group_id, success, fail) {
+  instance
+  .get(`groups/${group_id}`)
+  .then(success)
+  .catch(fail)
+}
+
+function getGroupfeeds(group_id, success, fail) {
+  instance
+  .get(`groups/${group_id}`)
+  .then(success)
+  .catch(fail)
+}
+
+// post 요청
+function createGroup(group, success, fail) {
+  instance
+  .post(`groups`, group)
+  .then(success)
+  .catch(fail)
+}
+
+function createGroupFeed(group_id, group_feed, success, fail) {
+  instance
+  .post(`groups/${group_id}/feeds`, group_feed)
+  .then(success)
+  .catch(fail)
+}
+
+function createGroupImg(group_id, goup_img, success, fail) {
+  instance
+  .post(`groups/${group_id}/image`, goup_img)
+  .then(success)
+  .catch(fail)
+}
+
+function joinGroup(group_id, success, fail) {
+  instance
+  .post(`groups/${group_id}/join`, goup_img)
+  .then(success)
+  .catch(fail)
+}
+
+// PUT 요청
+function updateGroupInfo(group_id, group, success, fail) {
+  instance
+    .put(`groups/${group_id}`, group)
+    .then(success)
+    .catch(fail);
+}
+
+function updateGroupFeed(group_id, group_feed_id, group_feed, success, fail) {
+  instance
+    .put(`groups/${group_id}/feeds/${group_feed_id}`, group_feed)
+    .then(success)
+    .catch(fail);
+}
+
+// Delete요청
+function deleteGroup(group_id, success, fail) {
+  instance
+  .delete(`groups/${group_id}`)
+  .then(success)
+  .catch(fail);
+}
+
+function secedeGroup(group_id, success, fail) {
+  instance
+  .delete(`groups/${group_id}/members`)
+  .then(success)
+  .catch(fail);
+}
+
+export{
+  getAllGroup, 
+  getGroupDetail, 
+  getGroupfeeds, 
+  createGroup, 
+  createGroupFeed, 
+  createGroupImg, 
+  joinGroup,
+  updateGroupInfo,
+  updateGroupFeed,
+  deleteGroup,
+  secedeGroup,
+}
