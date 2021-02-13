11bc74c14d891efe41af092fd1669c2959ffa115
zngsm <qbw0088@gmail.com>
2021-02-13 15:17:37 +0900

    [style] 기본 폰트 지정 + feed 수정
    - feed 노출시 날짜 ~~일전 형식으로 나오게 지정
    - main.css 에 기본 폰트 17px로 저장

---
+++ frontend/src/components/NewsFeed/Recommend.vue/MountainBanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="banner-header">
+      요즘 가장 핫한 도시 "{{ placeName }}" 으로 떠납니다
+    </div>
+    <div class="reco-body">
+      <NewsFeedMap :latitude="latitude" :longitude="longitude"/>
+    </div>
+    <div>
+      관련된 N개의 피드들이 있습니다.
+    </div>
+  </div>
+</template>
+
+<script>
+import NewsFeedMap from '../Common/NewsFeedMap.vue';
+export default {
+  components: { NewsFeedMap },
+  name: 'MountainBanner',
+  data() {
+    return {
+      placeName: '대전',
+      latitude: 36.353793856820566,
+      longitude:127.33999670291793,
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reco-body {
+  width: 100%;
+}
+</style>
